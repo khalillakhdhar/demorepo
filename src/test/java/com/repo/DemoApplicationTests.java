d8c79bc3cdbee6333041b061e7e670f9d86f5849
khalil <khalillakhdharatc@gmail.com>
2023-06-23 20:47:02 +0100

    Update DemoApplicationTests.java

--- src/test/java/com/repo/DemoApplicationTests.java
+++ src/test/java/com/repo/DemoApplicationTests.java
@@ -4,16 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.repo.repositories.EmployeeRepository;
 
 @SpringBootTest
 class DemoApplicationTests {
 	Validation validation;
+	@Mock 
+	EmployeeRepository empRepo;
 	@BeforeEach
 	void init()
 	{
 		validation=new Validation();
 	}
+
 	@Test
 	void validateChaine()
 	{
@@ -25,4 +32,10 @@ class DemoApplicationTests {
 		assertTrue(validation.validatePass("AZERTYuser"));
 	}
 	
+	@Test
+	void validateRepo()
+	{
+		assertTrue(empRepo.findByEmailAndPassword("khalil@test.com","user").getEmail().length()>0);
+	}
+
 }
